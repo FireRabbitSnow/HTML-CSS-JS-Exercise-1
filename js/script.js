879039ef8fc1afdb8409b52705adc856de24fd79
Renate W <Renate.Whitaker@gmail.com>
2022-05-19 05:34:57 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Practice exercise 1
+var footer = document.querySelector("footer");
+footer.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "Say it with flowers.";
+//console.log(tagline);
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fonstSize = "1.75em";
+//console.log(address);
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> of the week.";
+//console.log(everyDay);
